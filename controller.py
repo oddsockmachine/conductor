f85961636c7e140bf0bde0c1efd1df6e4ac6bcbd
David Walker <david.walker@anaplan.com>
2018-06-06 12:46:27 -0700

    midi clock in working, but timing is off

--- controller.py
+++ controller.py
@@ -15,18 +15,21 @@ mido.get_output_names()
 
 class Controller(object):
     """docstring for Controller."""
-    def __init__(self, stdscr, mport):
+    def __init__(self, stdscr, mport, mportin):
         super(Controller, self).__init__()
         self.mport = mport
+        self.mportin = mportin
         self.sequencer = Sequencer(mport)
         self.last = time()
         self.stdscr = stdscr
         self.cursor = Cursor()
         self.display = Display(stdscr)
+        self.beatclockcount = 0
 
     def run(self):
         while True:
-            if self.get_clock_tick():
+            # self.get_midi_tick()
+            if self.get_midi_tick():
                 self.sequencer.step_beat()
                 self.draw()
             key = self.get_keys()
@@ -65,6 +68,7 @@ class Controller(object):
         return str(c)
 
     def get_clock_tick(self):
+        return self.get_midi_tick()
         x = time()
         diff = x - self.last
         if diff > 0.3:  # 0.3 ms since last tick
@@ -72,30 +76,45 @@ class Controller(object):
             return True
         return False
 
+    def get_midi_tick(self):
+        for message in self.mportin.iter_pending():
+            if message.type == "clock":
+                # print('Received {}'.format(message))
+                self.beatclockcount += 1
+                # print(str(self.beatclockcount))
+        if self.beatclockcount > 6:
+            self.beatclockcount = 0
+            return True
+        return False
+
     def draw(self):
+        # pass
         self.sequencer.draw(self.stdscr)
         # self.cursor.draw(self.stdscr)
         self.stdscr.addstr(20, 40, "x{}, y{}  ".format(self.cursor.x, self.cursor.y))#, curses.color_pair(4))
+        self.stdscr.addstr(19, 40, str(self.beatclockcount))#, curses.color_pair(4))
+
         self.display.draw_cursor(self.cursor)
 
 
 def main(stdscr):
     stdscr.nodelay(1)
     with mido.open_output('Flynn', autoreset=True, virtual=True) as mport:
+        with mido.open_input('Flynn_In', autoreset=True, virtual=True) as mportin:
 
-        controller = Controller(stdscr, mport)
-        controller.sequencer.touch_note(1,3)
-        controller.sequencer.touch_note(1,4)
-        controller.sequencer.touch_note(1,6)
-        controller.sequencer.touch_note(2,6)
-        controller.sequencer.touch_note(2,6)
-        controller.sequencer.touch_note(3,1)
-        controller.sequencer.touch_note(3,4)
-        controller.sequencer.touch_note(4,7)
-        controller.sequencer.add_instrument("a", "major", octave=2, bars=4)
-        # controller.sequencer.add_instrument("c", "pentatonic", octave=4, bars=4)
-        # controller.sequencer.add_instrument("d", "pentatonic", octave=5, bars=4)
-        controller.run()
+            controller = Controller(stdscr, mport, mportin)
+            controller.sequencer.touch_note(1,3)
+            controller.sequencer.touch_note(1,4)
+            controller.sequencer.touch_note(1,6)
+            controller.sequencer.touch_note(2,6)
+            controller.sequencer.touch_note(2,6)
+            controller.sequencer.touch_note(3,1)
+            controller.sequencer.touch_note(3,4)
+            controller.sequencer.touch_note(4,7)
+            controller.sequencer.add_instrument("a", "major", octave=2, bars=4)
+            # controller.sequencer.add_instrument("c", "pentatonic", octave=4, bars=4)
+            # controller.sequencer.add_instrument("d", "pentatonic", octave=5, bars=4)
+            controller.run()
 
 
 if __name__ == '__main__':
