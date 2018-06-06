3cf1a7d11354d1feaa26786f569b51c66b51a847
David Walker <david.walker@anaplan.com>
2018-06-05 21:03:56 -0700

    note conversion works

--- note_conversion.py
+++ note_conversion.py
@@ -19,12 +19,24 @@ def get_full_scale(height, scale_name):
     times = int(height/len(scale)) + 1  # How many times to repeat the intervals to fill up the grid height
     return (scale * times)[:height]  # Repeat the intervals, then trim to fit
 
-def get_cell_to_note_converter():
-    pass
+def scale_to_offset(scale):
+    accum = 0
+    offsets = [0]
+    for s in scale[:-1]:
+        accum += s
+        offsets.append(accum)
+    return offsets
+
+def create_cell_to_midi_note_lookup(scale, octave, key, height):
+    starting_note = get_starting_note(octave, key)
+    offset = scale_to_offset(get_full_scale(height, scale))
+    notes = [x + starting_note for x in offset]
+    # print(notes)
+    lookup = {i: n for i,n in enumerate(notes)}
+    return lookup
 
 
 import unittest
-
 class TestStringMethods(unittest.TestCase):
 
     def test_starting_note(self):
@@ -44,8 +56,14 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(len(get_full_scale(16, "major")), 16)
         self.assertEqual(len(get_full_scale(16, "chromatic")), 16)
 
+    def test_scale_to_offset(self):
+        self.assertEqual(scale_to_offset(get_full_scale(16, "chromatic")), [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
+        self.assertEqual(scale_to_offset(get_full_scale(8, "chromatic")), [0,1,2,3,4,5,6,7])
+        self.assertEqual(scale_to_offset(get_full_scale(8, "pentatonic")), [0,3,5,8,10,12,15,17])
 
+    def test_lookup(self):
+        self.assertEqual(create_cell_to_midi_note_lookup("pentatonic", 2, "b", 4), {0: 47, 1: 50, 2: 52, 3: 55})
+        self.assertEqual(create_cell_to_midi_note_lookup("chromatic", 0, "a", 4), {0: 21, 1: 22, 2: 23, 3: 24})
 if __name__ == '__main__':
-    import unittest
 
     unittest.main()
