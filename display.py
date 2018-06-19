e5a88cec6621aa854b5716929a2ece7e017af00d
David Walker <david.walker@anaplan.com>
2018-06-19 14:19:34 -0700

    more gui components in windows

--- display.py
+++ display.py
@@ -24,10 +24,10 @@ class Display(object):
         self.stdscr.addstr(1, self.grid_offset_x+2, "S U P E R C E L L")#, curses.color_pair(4))
 
         # Box around the grid
-        rectangle(self.stdscr, self.grid_offset_y - 1,
-                               self.grid_offset_x - 1,
-                               self.grid_offset_y+self.grid_height,
-                               self.grid_offset_x+(2 * self.grid_width)+1)
+        # rectangle(self.stdscr, self.grid_offset_y - 1,
+        #                        self.grid_offset_x - 1,
+        #                        self.grid_offset_y+self.grid_height,
+        #                        self.grid_offset_x+(2 * self.grid_width)+1)
         status_strs = {
             'ins_num': status.get('ins_num').rjust(2),
             'ins_tot': status.get('ins_total').ljust(2),
@@ -45,16 +45,14 @@ class Display(object):
         self.stdscr.addstr(self.grid_height+self.grid_offset_y+2, self.grid_offset_x-1, status_line_1)#, curses.color_pair(4))
         self.stdscr.addstr(self.grid_height+self.grid_offset_y+3, self.grid_offset_x-1, status_line_2)#, curses.color_pair(4))
         self.stdscr.addstr(self.grid_height+self.grid_offset_y+4, self.grid_offset_x-1, button_line)#, curses.color_pair(4))
-        # for i in range(16):
-        #     self.stdscr.addstr(i+1, self.grid_offset_x+(self.grid_width*2)+5, DISPLAY[1])#, curses.color_pair(4))
-        # self.stdscr.addstr(int(status_strs['ins_num']), self.grid_offset_x+(self.grid_width*2)+5, DISPLAY[3])#, curses.color_pair(4))
         self.draw_ins_selector(int(status_strs['ins_num']), int(status_strs['ins_tot']))
+        self.draw_pages()
         return
 
     def draw_ins_selector(self, ins_num, ins_tot):
-        self.stdscr.addstr(1, self.grid_offset_x+2+(self.grid_width*2)+3, "I")#, curses.color_pair(4))
+        # self.stdscr.addstr(1, self.grid_offset_x+2+(self.grid_width*2)+3, "I")#, curses.color_pair(4))
         sx = self.grid_offset_x+(self.grid_width*2)+4
-        sy = self.grid_offset_y - 1
+        sy = self.grid_offset_y
         win = curses.newwin(ins_tot+2, 4, sy, sx)
         win.border()
         # Inactive instruments
@@ -70,29 +68,33 @@ class Display(object):
         win.border()
         win.refresh()
 
-    def draw_grid(self, led_grid):
+    def draw_grid(self, led_grid, cursor_pos):
         '''Take a led_grid/array from the sequencer and print it to the screen'''
+        sx = self.grid_offset_x
+        sy = self.grid_offset_y
+        win = curses.newwin(self.grid_height+2, (self.grid_width*2)+2, sy, sx)
+        win.border()
         for c, column in enumerate(led_grid):  # row counter
             for r, cell in enumerate(column):  # column counter
-                x = (c*2) + self.grid_offset_x
-                y = (H-r-1) + self.grid_offset_y
+                x = (c*2) + 1
+                y = (H-r-1) + 1
                 glyph = DISPLAY[cell]
-                self.stdscr.addstr(y, x, glyph)#, curses.color_pair(4))
+                win.addstr(y, x, glyph)#, curses.color_pair(4))
+        self.draw_cursor(win, cursor_pos)
+        win.refresh()
         return
 
-    def draw_cursor(self, cursor):
+    def draw_cursor(self, win, cursor):
         '''Draw the cursor over the grid'''
-        x = (cursor['x']*2) + self.grid_offset_x
-        y = (self.grid_height-cursor['y']-1) + self.grid_offset_y
+        x = (cursor['x']*2) + 1
+        y = (self.grid_height-cursor['y']-1) + 1
         glyph = DISPLAY[LED_CURSOR]
-        self.stdscr.addstr(y, x, glyph)#, curses.color_pair(4))
+        win.addstr(y, x, glyph)#, curses.color_pair(4))
         return
 
     def draw_all(self, status, led_grid, cursor_pos):
         self.draw_gui(status)
-        self.draw_grid(led_grid)
-        self.draw_cursor(cursor_pos)
-        self.draw_pages()
+        self.draw_grid(led_grid, cursor_pos)
         return
 
 
