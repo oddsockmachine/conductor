909004a11f453ef7ffdb10ba6e94af8bd3c27566
Dave Walker <davidwalker@Davids-MacBook-Pro.local>
2018-12-10 18:51:36 -0800

    fix

--- hardware.py
+++ hardware.py
@@ -18,8 +18,8 @@ class Display(object):
 
         self.grid_h = h
         self.grid_w = w
-        self.led_matrix = [[(0,0,0) for x  in w] for y in h]
-        self.old_led_matrix = [[(0,0,0) for x  in w] for y in h]
+        self.led_matrix = [[(0,0,0) for x  in range(len(w))] for y in range(len(h))]
+        self.old_led_matrix = [[(0,0,0) for x  in range(len(w))] for y in range(len(h))]
         return
 
     def get_cmds(self):
